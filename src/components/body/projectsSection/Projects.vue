f17c233011a65ddb793e2817824fec09d444cb49
Fabio-GitHub25 <fabio.eletto@web.de>
2023-04-07 16:47:09 +0200

    changed twitter profile to stackoverflow profile + removed external helix url

--- src/components/body/projectsSection/Projects.vue
+++ src/components/body/projectsSection/Projects.vue
@@ -1,13 +1,21 @@
 <template>
   <Section :sectionName="sectionName">
     <div class="w-full">
-      <div class="grid grid-cols-1 grid-rows-3 md:grid-cols-7 md:grid-rows-4 md:gap-x-8 gap-y-8">
+      <div
+        class="grid grid-cols-1 grid-rows-3 md:grid-cols-7 md:grid-rows-4 md:gap-x-8 gap-y-8"
+      >
         <div class="md:col-start-1 md:col-span-3 md:row-span-2">
           <Card
             :title="'HelixScripts'"
             :routeLink="'helix-scripts'"
-            :usedTechnologies="['JavaScript', 'Vue.js', 'HTML', 'Python', 'Flask', 'Webpack']"
-            :externalPageUrl="'https://helixscripts.com/'"
+            :usedTechnologies="[
+              'JavaScript',
+              'Vue.js',
+              'HTML',
+              'Python',
+              'Flask',
+              'Webpack',
+            ]"
           >
             Automated checkout solution for different online stores
           </Card>
@@ -16,7 +24,9 @@
           <Card
             :title="'Accounting script'"
             :usedTechnologies="['Python', 'Stripe API', 'sevDesk API']"
-            :gitUrl="'https://github.com/Fabio-GitHub25/stripe_sevdesk_accounting_script'"
+            :gitUrl="
+              'https://github.com/Fabio-GitHub25/stripe_sevdesk_accounting_script'
+            "
           >
             Automated booking of stripe payout invoices in sevDesk
           </Card>
@@ -25,7 +35,9 @@
           <Card
             :title="'Image uploader'"
             :usedTechnologies="['Node.js', 'Discord.js', 'Twitter API']"
-            :gitUrl="'https://github.com/Fabio-GitHub25/discord_twitter_image_uploader'"
+            :gitUrl="
+              'https://github.com/Fabio-GitHub25/discord_twitter_image_uploader'
+            "
           >
             Automatic image uploader from Discord to Twitter
           </Card>
@@ -40,13 +52,13 @@ import Card from "./Card.vue";
 export default {
   components: {
     Section,
-    Card
+    Card,
   },
   props: {
     sectionName: {
       type: String,
       required: true,
-    }
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Main from './Main.vue';
+import Vue from "vue";
+import Main from "./Main.vue";
 import router from "./router";
-import './main.css';
+import "./main.css";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import specific icons */
 import {
@@ -16,12 +16,16 @@ import {
   faArrowLeft,
   faArrowCircleDown,
   faArrowUpRightFromSquare,
-} from '@fortawesome/free-solid-svg-icons'
-import { } from '@fortawesome/free-regular-svg-icons'
-import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+} from "@fortawesome/free-solid-svg-icons";
+import {} from "@fortawesome/free-regular-svg-icons";
+import {
+  faGithub,
+  faTwitter,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* add icons to the library */
 library.add(
@@ -34,17 +38,17 @@ library.add(
   faEnvelope,
   faArrowLeft,
   faArrowCircleDown,
-  faArrowUpRightFromSquare
-)
+  faArrowUpRightFromSquare,
+  faStackOverflow
+);
 
 /* add font awesome icon component */
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  render: h => h(Main)
+  render: (h) => h(Main),
 });
-
